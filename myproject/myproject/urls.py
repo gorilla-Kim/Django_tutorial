ce4decec782a07640059fcbcb73da44b87d47f2e
gorilla-Kim <00ghks22@naver.com>
2019-01-09 03:11:10 +0900

    django tutorial

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import myapp.views
 
 urlpatterns = [
+    path('', myapp.views.home, name="home"),
     path('admin/', admin.site.urls),
 ]
