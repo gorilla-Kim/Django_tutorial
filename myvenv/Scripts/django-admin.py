7ac8f921ceafb4323556190898ea5338e5c6fa8c
gorilla-Kim <00ghks22@naver.com>
2019-01-09 01:36:29 +0900

    venv & my first django project

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\administrator\desktop\blog\gorilla_lab\back-end공방\django\django_tutorial\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
