0d064480276938122ea99be2e7c61a454e3da126
gorilla-Kim <00ghks22@naver.com>
2019-01-12 02:27:24 +0900

    upload favicon

--- example1/elections/urls.py
+++ example1/elections/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path, include
 from . import views
 
+app_name = 'elections'
+
 urlpatterns = [
     #elections App의 views로 연결
-    path('', views.index),
+    path('', views.index, name = 'home'),
     path('candidate/', views.candidate),
     re_path('candidate/areas/(?P<area>.+)/$', views.areas),
     re_path('candidate/areas/(?P<area>.+)/results$', views.results),
